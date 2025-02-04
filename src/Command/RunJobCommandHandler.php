81da44614d990b1dd8750b3ea21e5854507a610f
hissy <hishikawa@macareux.co.jp>
2025-02-04 18:01:46 +0900

    Add output messages

--- src/Command/RunJobCommandHandler.php
+++ src/Command/RunJobCommandHandler.php
@@ -2,11 +2,15 @@
 
 namespace Macareux\JobRunner\Command;
 
+use Concrete\Core\Command\Task\Output\OutputAwareInterface;
+use Concrete\Core\Command\Task\Output\OutputAwareTrait;
 use Concrete\Core\Support\Facade\Application;
 use Macareux\JobRunner\Job\Service;
 
-class RunJobCommandHandler
+class RunJobCommandHandler implements OutputAwareInterface
 {
+    use OutputAwareTrait;
+
     public function __invoke(RunJobCommand $command)
     {
         $app = Application::getFacadeApplication();
@@ -14,6 +18,7 @@ class RunJobCommandHandler
         $service = $app->make(Service::class);
         $job = $service->getJobByHandle($command->getJobHandle());
         if ($job) {
+            $this->output->write('Running job: ' . $command->getJobHandle());
             $job->run();
         }
     }
--- src/Command/RunJobQueueCommandHandler.php
+++ src/Command/RunJobQueueCommandHandler.php
@@ -2,17 +2,22 @@
 
 namespace Macareux\JobRunner\Command;
 
+use Concrete\Core\Command\Task\Output\OutputAwareInterface;
+use Concrete\Core\Command\Task\Output\OutputAwareTrait;
 use Concrete\Core\Job\Job;
 use Macareux\JobRunner\Job\QueueableJob;
 use Macareux\JobRunner\Queue\Message;
 
-class RunJobQueueCommandHandler
+class RunJobQueueCommandHandler implements OutputAwareInterface
 {
+    use OutputAwareTrait;
+
     public function __invoke(RunJobQueueCommand $command)
     {
         $jobHandle = $command->getJobHandle();
         $job = Job::getJobObjByHandle($jobHandle);
         if ($job instanceof QueueableJob) {
+            $this->output->write('Processing job queue: ' . $jobHandle);
             $queueBody = $command->getBody();
             $queueMessage = new Message($queueBody);
             $job->processQueueItem($queueMessage);
